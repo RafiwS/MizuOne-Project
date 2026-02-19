846fdbc35bd7f39d9f857f53ca8eed36dd5b5208
RafiwS <77223923+RafiwS@users.noreply.github.com>
2026-02-19 19:49:58 +0700

    Update assets.ts

--- mizuone-landing-page/src/constants/assets.ts
+++ mizuone-landing-page/src/constants/assets.ts
@@ -1,8 +1,8 @@
 export const ASSETS = {
-  logo: "https://www.figma.com/api/mcp/asset/ec258767-ed4c-431a-9ca4-72876302e97e",
+  logo: "/logo.png",
   logoAlt: "https://www.figma.com/api/mcp/asset/3b2a4955-ea49-455e-bfbf-56395c82ec1e",
-  machine: "https://www.figma.com/api/mcp/asset/2cee224c-4a9a-4b7f-9f50-5bca1331c4cf",
-  droplet: "https://www.figma.com/api/mcp/asset/1d81e20a-e038-4887-8924-5c83c25470b8",
+  machine: "/vending_machine.png",
+  droplet: "/Droplet.png",
   waterBg: "https://www.figma.com/api/mcp/asset/dde95667-3fee-48f6-8be5-da94181b76b0",
   arrowRight: "https://www.figma.com/api/mcp/asset/713b8321-e10b-4466-a42c-a398d7ce1df1",
   subtract: "https://www.figma.com/api/mcp/asset/54da9eb7-76a4-4be5-a7fc-ff4cbb51e271",
